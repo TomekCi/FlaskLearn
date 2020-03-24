c849c3189bc0e14e4d4fe487cc8597dff023a8f6
TomekCi <tomekciesz@gmail.com>
2020-03-24 22:50:01 +0100

    Finished Flask tutorial

--- PycharmProjects/FlaskLearn/FlaskLearnEnv/FlaskApp.py
+++ PycharmProjects/FlaskLearn/FlaskLearnEnv/FlaskApp.py
@@ -36,20 +36,56 @@ def index():
 @app.route('/posts', methods=['GET', 'POST'])
 def posts():
     if request.method == 'POST':
-        return redirect('/')
         post_title = request.form['title']
         post_content = request.form['content']
-        new_post = BlogPost(title = post_title, content = post_content, author = 'Tom')
+        post_author = request.form['author']
+        new_post = BlogPost(title = post_title, content = post_content, author = post_author)
         db.session.add(new_post)
         db.session.commit()
         return redirect('/posts')
     elif request.method == 'GET':
-    #    all_posts = BlogPost.query.order_by(BlogPost.date_posted).all()
+        all_posts = BlogPost.query.order_by(BlogPost.date_posted).all()
         return render_template('posts.html', posts=all_posts)
+
+@app.route('/posts/delete/<int:id>')
+def delete(id):
+    post = BlogPost.query.get_or_404(id)
+    db.session.delete(post)
+    db.session.commit()
+    return redirect('/posts')
+
+@app.route('/posts/edit/<int:id>', methods=['GET', 'POST'])
+def edit(id):
+
+    post = BlogPost.query.get_or_404(id)
+
+    if request.method == 'POST':
+        post.title = request.form['title']
+        post.content = request.form['content']
+        post.author = request.form['author']
+        db.session.commit()
+        return redirect('/posts')
+    else:
+        return render_template('edit.html', post=post)
 
 @app.route('/home/<string:name>')
 def hello(name):
     return "Hello " + name
+
+@app.route('/posts/new', methods=['GET','POST'])
+def new_posts():
+    if request.method == 'POST':
+        post_title = request.form['title']
+        post_content = request.form['content']
+        post_author = request.form['author']
+        new_post = BlogPost(title = post_title, content = post_content, author = post_author)
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect('/posts')
+    elif request.method == 'GET':
+        all_posts = BlogPost.query.order_by(BlogPost.date_posted).all()
+        return render_template('new_post.html', posts=all_posts)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
